6c574dbaf022f449d0a7ab16ab194e3c1a48b273
canaydindev <canaydindev@gmail.com>
2025-04-08 21:11:06 +0300

    v1.0

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { Separator } from "@/components/ui/separator";
 import CihazMotionPage from "@/models/home/cihazlar-paketler/CihazMotionPage";
 import DidYouKnowPage from "@/models/home/did-you-know/didYouKnowPage";
 import Welcome from "@/models/home/home-first/Welcome";
 import { HomeNavbar } from "@/models/home/home-navbar/HomeNavbar";
+import KampanyaPage from "@/models/home/kampanya/kampanyaPage";
 import NasilmiPage from "@/models/home/nasil-mi/nasilmiPage";
 
 const Page = () => {
@@ -9,10 +11,14 @@ const Page = () => {
     <main className="scroll-smooth">
       <HomeNavbar />
       <Welcome />
-
+      <KampanyaPage />
+      <Separator />
       <NasilmiPage />
+      <Separator />
       <CihazMotionPage />
+
       <DidYouKnowPage />
+      <Separator />
     </main>
   );
 };
